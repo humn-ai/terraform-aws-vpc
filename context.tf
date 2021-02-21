f3defa61d0d80bc76a21118871da23f31e013a91
Callum <callumccr@gmail.com>
2021-02-21 21:39:53 +0000

    updated the label module for additional attributes

--- context.tf
+++ context.tf
@@ -18,7 +18,29 @@
 # will be null, and `module.this.delimiter` will be `-` (hyphen).
 #
 
-module "this" {
+module "vpc_label" {
+  source  = "cloudposse/label/null"
+  version = "0.24.1" # requires Terraform >= 0.13.0
+
+  enabled             = var.enabled
+  namespace           = var.namespace
+  environment         = var.environment
+  stage               = var.stage
+  name                = var.name
+  delimiter           = var.delimiter
+  attributes          = var.attributes
+  tags                = var.tags
+  additional_tag_map  = var.additional_tag_map
+  label_order         = var.label_order
+  regex_replace_chars = var.regex_replace_chars
+  id_length_limit     = var.id_length_limit
+  label_key_case      = var.label_key_case
+  label_value_case    = var.label_value_case
+
+  context = var.context
+}
+
+module "igw_label" {
   source  = "cloudposse/label/null"
   version = "0.24.1" # requires Terraform >= 0.13.0
 
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  enabled                                         = module.this.enabled
+  enabled                                         = module.vpc.enabled
   enable_default_security_group_with_custom_rules = var.enable_default_security_group_with_custom_rules && local.enabled ? 1 : 0
   enable_internet_gateway                         = var.enable_internet_gateway && local.enabled ? 1 : 0
   additional_cidr_blocks_defined                  = local.enabled && var.additional_cidr_blocks != null ? true : false
@@ -10,7 +10,7 @@ module "label" {
   source  = "cloudposse/label/null"
   version = "0.24.1"
 
-  context = module.this.context
+  context = module.vpc.context
 }
 
 resource "aws_vpc" "default" {
@@ -22,7 +22,7 @@ resource "aws_vpc" "default" {
   enable_classiclink               = var.enable_classiclink
   enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
-  tags                             = module.label.tags
+  tags                             = module.vpc_label.tags
 }
 
 # If `aws_default_security_group` is not defined, it would be created implicitly with access `0.0.0.0/0`
@@ -36,7 +36,7 @@ resource "aws_default_security_group" "default" {
 resource "aws_internet_gateway" "default" {
   count  = local.enable_internet_gateway
   vpc_id = join("", aws_vpc.default.*.id)
-  tags   = module.label.tags
+  tags   = module.igw_label.tags
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "default" {
